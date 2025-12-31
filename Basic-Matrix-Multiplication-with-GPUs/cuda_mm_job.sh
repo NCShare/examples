a1e0c7c10ef6228d2c4317feba8d84cff69c36ba
Uthpala Herath <uthpala.herath@duke.edu>
2025-12-30 20:47:40 -0500

    Updated Matrix multiplication with job script

---
+++ Basic-Matrix-Multiplication-with-GPUs/cuda_mm_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -J matrix-multiplication    # Job name
+#SBATCH -p gpu                      # Partition name
+#SBATCH --gres=gpu:h200:1           # Number of GPUs
+#SBATCH --mem=20G                   # Memory per node
+
+conda activate <env_name_with_pytorch>
+
+cd $SLURM_SUBMIT_DIR
+./cuda_mm.py > cuda_mm.log 2>&1
