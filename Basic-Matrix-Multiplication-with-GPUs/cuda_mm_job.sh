a452c1c07156811b3789cffb04f39e4b7c870460
Uthpala Herath <uthpala.herath@duke.edu>
2025-12-30 21:11:38 -0500

    Added source .bashrc and chmod lines to job script

--- Basic-Matrix-Multiplication-with-GPUs/cuda_mm_job.sh
+++ Basic-Matrix-Multiplication-with-GPUs/cuda_mm_job.sh
@@ -4,7 +4,9 @@
 #SBATCH --gres=gpu:h200:1           # Number of GPUs
 #SBATCH --mem=20G                   # Memory per node
 
+source ~/.bashrc
 conda activate <env_name_with_pytorch>
 
 cd $SLURM_SUBMIT_DIR
+chmod +x cuda_mm.py
 ./cuda_mm.py > cuda_mm.log 2>&1
